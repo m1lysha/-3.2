f1452174e49a672843c32a65684d6c0de1705609
RobotComp.ru <RobotComp.ru@DESKTOP-94TEC7C>
2024-11-07 22:06:54 +0300

    Добавьте файлы проекта.

---
+++ лаб 3.2/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Console.Write("Введите число m: ");
+        int m = Convert.ToInt32(Console.ReadLine());
+
+        if (m == 0)
+        {
+            Console.WriteLine("Ошибка: Деление на ноль недопустимо.");
+            return;
+        }
+
+       
+        int[] oddHundreds = { 100, 300, 500, 700, 900 };
+
+       
+        Console.WriteLine("Результаты целочисленного деления нечетных сотен на m:");
+        foreach (int number in oddHundreds)
+        {
+            int result = number / m;
+            Console.WriteLine($"{number} / {m} = {result}");
+        }
+    }
+}
